efd213e79df38602ed56bf371a280dd69ddf2905
jy562 <127701590+jy562@users.noreply.github.com>
2025-07-22 09:19:39 -0700

    presentation

--- MATLAB/example_fft.m
+++ MATLAB/example_fft.m
@@ -1,4 +1,14 @@
 % AA REU Workshop Tutorial Code
+% DFT and FFT.
 
 clear; clc; close all;
 
+%% DFT matrix:
+
+N = 256;
+omega_n = exp(-2i*pi/N);
+[I, J] = meshgrid(1:N, 1:N);
+DFT_mat = omega_n.^((I-1).*(J-1));
+
+imagesc(real(DFT_mat));
+colormap jet; colorbar;
